38661c3c46051a87b79cdb3108437c82e033db7b
Nikolai Pawlowski <nikolaip@gmail.com>
2017-12-23 06:17:19 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,7 +34,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |element, i|
+  array.collect.with_index do |element, i|
     i != 1 ? element << "s" : element
   end
 end
